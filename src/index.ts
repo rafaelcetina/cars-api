2225a28146b28d1291f53efe45cefb32a4400cce
Rodolfo Dzul <rdzul@thepalacecompany.com>
2025-03-18 21:56:18 -0600

    Agregar configuración de dotenv y utilizar variable de entorno para el puerto

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import express from 'express' // ESModules
-
+import dotenv from 'dotenv'
 import { brandRoutes } from './routes/brands'
+
+dotenv.config()
 
 const app = express()
 app.use(express.json()) // middleware que transforma la req.body a un json
 
-const PORT = 3000
+const PORT = process.env.PORT ?? 3000
 
 app.get('/ping', (_req, res) => {
   console.log('someone pinged here!!')
